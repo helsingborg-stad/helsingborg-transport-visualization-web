b8c39fe074b6bc23c89504f8e4f669ab486fd914
Josephine <josephine.hedman@vntrs.com>
2023-05-08 11:25:35 +0200

    add active filter indicator

--- src/components/FilterButton/FilterBox/FilterBox.tsx
+++ src/components/FilterButton/FilterBox/FilterBox.tsx
@@ -12,21 +12,32 @@ type FilterBoxProps = {
   triggerReload: () => void;
 };
 
-// TODO: add on click for Visa button in action bar
 export const FilterBox: FC<FilterBoxProps> = ({
   close, label, children, clearFilter, triggerReload,
-}) => (
-  <Styled.Container>
-    <Styled.Header>
-      <Styled.Heading>{label}</Styled.Heading>
-      <Styled.IconWrapper src={CloseIcon} alt="close" onClick={close} />
-    </Styled.Header>
-    <Styled.FilterContainer>
-      {children}
-    </Styled.FilterContainer>
-    <Styled.ActionBar>
-      <Button tertiary buttonSize={ButtonSize.SMALL} onClick={clearFilter}>Rensa</Button>
-      <Button primary buttonSize={ButtonSize.SMALL} onClick={triggerReload}>Visa</Button>
-    </Styled.ActionBar>
-  </Styled.Container>
-);
+}) => {
+  const showFilter = () => {
+    triggerReload();
+    close();
+  };
+
+  const clearAndClose = () => {
+    clearFilter();
+    close();
+  };
+
+  return (
+    <Styled.Container>
+      <Styled.Header>
+        <Styled.Heading>{label}</Styled.Heading>
+        <Styled.IconWrapper src={CloseIcon} alt="close" onClick={close} />
+      </Styled.Header>
+      <Styled.FilterContainer>
+        {children}
+      </Styled.FilterContainer>
+      <Styled.ActionBar>
+        <Button tertiary buttonSize={ButtonSize.SMALL} onClick={clearAndClose}>Rensa</Button>
+        <Button primary buttonSize={ButtonSize.SMALL} onClick={showFilter}>Visa</Button>
+      </Styled.ActionBar>
+    </Styled.Container>
+  );
+};
