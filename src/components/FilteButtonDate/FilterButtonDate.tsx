4e2de791ca4abf94030c13b86ad7621cc6502435
Josephine <josephine.hedman@vntrs.com>
2023-05-08 14:45:51 +0200

    updates filterbutton component styling

--- src/components/FilteButtonDate/FilterButtonDate.tsx
+++ src/components/FilteButtonDate/FilterButtonDate.tsx
@@ -1,8 +1,6 @@
 import { FC, useRef, useState } from 'react';
 import ExpandArrow from 'assets/expand_arrow_icon.svg';
-import { ButtonSize } from 'components/Button/types';
 import { useHandleClickOutside } from 'hooks';
-import { Button } from '../Button';
 import { FilterBox } from './FilterBox';
 import * as Styled from './styled';
 
@@ -21,15 +19,14 @@ export const FilterButtonDate: FC<FilterButtonDateProps> = ({ label }) => {
     },
   });
 
-  // TODO: add active state for filter button (styling)!!
   return (
     <Styled.Container ref={containerRef}>
-      <Button type="button" outline buttonSize={ButtonSize.SMALL} onClick={() => setIsOpen(!isOpen)}>
+      <Styled.Button type="button" outline onClick={() => setIsOpen(!isOpen)}>
         <Styled.Content>
           {label}
           <img src={ExpandArrow} alt="arrow icon" />
         </Styled.Content>
-      </Button>
+      </Styled.Button>
       {isOpen && (
         <FilterBox />
       )}
--- src/components/FilterButton/styled.ts
+++ src/components/FilterButton/styled.ts
@@ -1,5 +1,4 @@
-import styled from 'styled-components';
-import { Button as ButtonComponent } from '../Button';
+import styled, { css } from 'styled-components';
 
 export const Container = styled.div`
   position: relative;
@@ -11,6 +10,42 @@ export const Content = styled.div`
   gap: 2px;
 `;
 
-export const Button = styled(ButtonComponent)`
-  background-color: red;
+type ButtonProps = {
+  primary?: boolean;
+  outline?: boolean;
+  isActive?: boolean;
+} & React.HTMLProps<HTMLButtonElement>;
+
+export const Button = styled.button<ButtonProps>`
+  --button-background-color: var(--color-black);
+  --button-color: var(--color-white);
+  
+  border-radius: var(--border-radius-md);
+  font-family: var(--font-family);
+  background: var(--button-background-color);
+  color: var(--button-color);
+  cursor: pointer;
+  font-weight: var(--font-weight-400);
+  font-size: 12px;
+  width: fit-content;
+  line-height: var(--line-height-xxxs);
+  padding: 7px 14px;
+
+  &:disabled {
+    background-color: grey;
+  }
+
+  ${({ outline }) => outline
+  && css`
+    border: 1px solid var(--color-gray-7);
+    --button-background-color: var(--color-white);
+    --button-color: var(--color-black);
+  `}
+
+  ${({ isActive }) => isActive
+  && css`
+    border: 1px solid var(--color-black);
+    --button-background-color: var(--color-white);
+    --button-color: var(--color-black);
+  `}
 `;
