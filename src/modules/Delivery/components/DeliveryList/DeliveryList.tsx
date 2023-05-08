8241d8134d7d0bf54cd53d7c37f1b0226b02bd5a
Josephine <josephine.hedman@vntrs.com>
2023-05-08 14:45:31 +0200

    adds event api call to fetch delivery data

--- src/modules/Delivery/components/DeliveryList/DeliveryList.tsx
+++ src/modules/Delivery/components/DeliveryList/DeliveryList.tsx
@@ -1,12 +1,17 @@
+import { useGetEvents } from 'modules/Delivery/hooks';
 import { FilterList } from './FilterList';
 import { ListContent } from './ListContent';
 import { ListHeader } from './ListHeader';
 import * as Styled from './styled';
 
-export const DeliveryList = () => (
-  <Styled.Container>
-    <FilterList />
-    <ListHeader />
-    <ListContent />
-  </Styled.Container>
-);
+export const DeliveryList = () => {
+  const { fetchEvents, events } = useGetEvents();
+
+  return (
+    <Styled.Container>
+      <FilterList fetchEvents={fetchEvents} />
+      <ListHeader />
+      <ListContent events={events} />
+    </Styled.Container>
+  );
+};
--- src/modules/Delivery/hooks/index.ts
+++ src/modules/Delivery/hooks/index.ts
@@ -1,1 +1,2 @@
 export * from './useGetEventFilters';
+export * from './useGetEvents';
