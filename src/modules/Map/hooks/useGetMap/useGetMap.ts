51779c8e3132a1de0ea166d9da3570ecf1d95efa
Dennis Hadzialic <25294013+D3nnis38@users.noreply.github.com>
2023-05-15 17:15:43 +0200

    implement new cluster and marker functionallity

--- src/modules/Map/hooks/useGetMap/useGetMap.ts
+++ src/modules/Map/hooks/useGetMap/useGetMap.ts
@@ -1,20 +1,21 @@
+// @ts-nocheck -- no types for packages
 import { useEffect, useRef, useState } from 'react';
 import { Loader } from '@googlemaps/js-api-loader';
-// eslint-disable-next-line import/no-extraneous-dependencies
 import { MarkerClusterer } from '@googlemaps/markerclusterer';
-// @ts-ignore
-// eslint-disable-next-line import/no-extraneous-dependencies
 import centerOfMass from '@turf/center-of-mass';
 import { useZoneApi } from 'hooks/useZoneApi';
 import { FeatureCollection } from 'types/zone';
 import { Organisation } from 'types/organisation';
 import DistributionZoneIcon from 'assets/distribution_zone.svg';
+import DistributionZoneIconInverted from 'assets/distribution_zone_inverted.svg';
 import DeliveryZoneIcon from 'assets/delivery_zone.svg';
+import DeliveryZoneIconInverted from 'assets/delivery_zone_inverted.svg';
+import { ZoneType } from 'types/index';
 
 const { VITE_GOOGLE_MAPS_API_KEY } = import.meta.env;
 
 export const useGetMap = () => {
-  const { getAllZones } = useZoneApi();
+  const { getAllZones, getRelatedDistributionZones, getRelatedDeliveryZones } = useZoneApi();
   const [error, setError] = useState<string>();
   const [isLoading, setIsLoading] = useState<boolean>();
   const [zones, setZones] = useState<FeatureCollection>();
@@ -69,6 +70,32 @@ export const useGetMap = () => {
           setIsLoading(false);
           return;
         }
+
+        const markCluster = new MarkerClusterer({ map });
+        function removeMarkerFromCluster(marker: google.maps.Marker) {
+          markCluster.removeMarker(marker);
+        }
+
+        function addMarkerToCluster(marker: google.maps.Marker) {
+          markCluster.addMarker(marker);
+        }
+
+        function invertIcon(m: google.maps.Marker): void {
+          if (m.getIcon() === DeliveryZoneIcon) {
+            m.setIcon(DeliveryZoneIconInverted);
+          } else if (m.getIcon() === DistributionZoneIcon) {
+            m.setIcon(DistributionZoneIconInverted);
+          }
+        }
+
+        function revertIcon(m: google.maps.Marker): void {
+          if (m.getIcon() === DeliveryZoneIconInverted) {
+            m.setIcon(DeliveryZoneIcon);
+          } else if (m.getIcon() === DistributionZoneIconInverted) {
+            m.setIcon(DistributionZoneIcon);
+          }
+        }
+
         const infoWindow = new google.maps.InfoWindow({
           pixelOffset: new google.maps.Size(0, -30),
         });
@@ -78,26 +105,63 @@ export const useGetMap = () => {
           const marker = new google.maps.Marker({
             position: { lng: center[0], lat: center[1] },
             map,
-            icon: zone.properties.type === 'distribution' ? DistributionZoneIcon : DeliveryZoneIcon,
+            icon: zone.properties.type === ZoneType.DISTRIBUTION
+              ? DistributionZoneIcon
+              : DeliveryZoneIcon,
           });
 
-          marker.addListener('click', (event: google.maps.Data.MouseEvent) => {
+          marker.id = zone.properties.id;
+
+          marker.addListener('click', async (event: google.maps.Data.MouseEvent) => {
             if (event.latLng) {
               const { name, organisation } = zone.properties;
-
               infoWindow.setContent(InfoBox(organisation, name));
               infoWindow.setPosition(
                 new google.maps.LatLng(event.latLng.lat(), event.latLng.lng()),
               );
-
               infoWindow.open(map);
+              invertIcon(marker);
+              const relatedZones = zone.properties.type === ZoneType.DELIVERY
+                ? await getRelatedDistributionZones(zone.properties.id)
+                : await getRelatedDeliveryZones(zone.properties.id);
+              if (relatedZones.data.features === null) {
+                markers.forEach((m) => {
+                  if (m !== marker) {
+                    removeMarkerFromCluster(m);
+                  } else {
+                    invertIcon(m);
+                  }
+                });
+              }
+              const arrayOfZoneIds = relatedZones.data.features.map((z) => z.properties.id);
+              markers.forEach((m) => {
+                if (m.id !== zone.properties.id) {
+                  removeMarkerFromCluster(m);
+                }
+                if (arrayOfZoneIds.includes(m.id)) {
+                  addMarkerToCluster(m);
+                  invertIcon(m);
+                }
+              });
             }
+          });
+          google.maps.event.addListener(infoWindow, 'closeclick', () => {
+            markers.forEach((m) => {
+              addMarkerToCluster(m);
+              revertIcon(m);
+            });
+          });
+          google.maps.event.addListener(map, 'click', () => {
+            infoWindow.close();
+            markers.forEach((m) => {
+              addMarkerToCluster(m);
+              revertIcon(m);
+            });
           });
           return marker;
         });
-        // @ts-ignore
-        // eslint-disable-next-line @typescript-eslint/no-unused-vars
-        const markCluster = new MarkerClusterer({ map, markers });
+
+        markCluster.addMarkers(markers);
       })
       .catch((err) => console.log('Err occurred while loading Google Maps', err))
       .finally(() => setIsLoading(false));
