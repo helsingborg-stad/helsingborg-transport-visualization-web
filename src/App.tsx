d3be14a8429bc95b96514e6ebe930a06646d7fcd
Dennis Hadzialic <25294013+D3nnis38@users.noreply.github.com>
2023-05-23 14:38:21 +0200

    test push

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,8 @@ import { Router } from './modules';
 import { GlobalStyle } from './theme';
 import { AuthContextProvider } from './contexts';
 import './App.css';
-
+// eslint-disable-next-line spaced-comment
+//testing
 export const App = () => (
   <AuthContextProvider>
     <Router />
