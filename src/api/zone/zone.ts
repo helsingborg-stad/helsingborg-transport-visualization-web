2b6eb8bcd7ca7d185ce28b92a2981232d6fea1f1
Dennis Hadzialic <25294013+D3nnis38@users.noreply.github.com>
2023-07-04 23:24:48 +0200

    add getZonesByOrganisation api

--- src/api/zone/zone.ts
+++ src/api/zone/zone.ts
@@ -2,5 +2,6 @@ import { FeatureCollection } from 'types/zone';
 import { client } from '../baseAxios';
 
 export const getZones = () => client.get<FeatureCollection>('/zones/');
+export const getZonesByOrganisation = (id: string) => client.get<FeatureCollection>(`/organisation/${id}zones`);
 export const getDistributionZones = (id: string) => client.get<FeatureCollection>(`/zones/${id}/distribution`);
 export const getDeliveryZones = (id: string) => client.get<FeatureCollection>(`/zones/${id}/delivery`);
--- src/hooks/useZoneApi.ts
+++ src/hooks/useZoneApi.ts
@@ -1,12 +1,16 @@
-import { getZones, getDistributionZones, getDeliveryZones } from 'api/zone';
+import {
+  getZones, getDistributionZones, getDeliveryZones, getZonesByOrganisation,
+} from 'api/zone';
 
 export const useZoneApi = () => {
   const getAllZones = () => getZones();
+  const getAllZonesByOrganisation = (id: string) => getZonesByOrganisation(id);
   const getRelatedDistributionZones = (id: string) => getDistributionZones(id);
   const getRelatedDeliveryZones = (id: string) => getDeliveryZones(id);
 
   return {
     getAllZones,
+    getAllZonesByOrganisation,
     getRelatedDistributionZones,
     getRelatedDeliveryZones,
   };
