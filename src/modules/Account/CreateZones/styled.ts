739726645d34c21d2ca8aecb777c63ee362fa700
Dennis Hadzialic <25294013+D3nnis38@users.noreply.github.com>
2023-07-12 12:06:10 +0200

    add create zones site

---
+++ src/modules/Account/CreateZones/styled.ts
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+
+export const ContentContainer = styled.div`
+  padding: 36px 32px;
+  padding-bottom: 64px;
+`;
+
+export const FormContainer = styled.div`
+  width: 450px;
+  margin: 24px auto 32px;
+`;
+
+export const Header = styled.h1`
+  font-size: var(--font-size-heading-xs);
+  font-weight: var(--font-weight-800);
+  line-height: var(--line-height-xxxl);
+  margin-bottom: var(--spacing-xxs);
+  font-family: var(--font-family);
+`;
