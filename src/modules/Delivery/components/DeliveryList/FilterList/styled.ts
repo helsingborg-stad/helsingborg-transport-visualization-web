653f72eb791f100ac9e658c0f12d7e48681dc8ff
Dennis Hadzialic <25294013+D3nnis38@users.noreply.github.com>
2024-03-12 10:58:52 +0100

    Add exportEventsToExcel functionality to DeliveryList

--- src/modules/Delivery/components/DeliveryList/FilterList/styled.ts
+++ src/modules/Delivery/components/DeliveryList/FilterList/styled.ts
@@ -1,6 +1,11 @@
 import styled from 'styled-components';
 
-export const Container = styled.div`
+export const FilterContainer = styled.div`
   display: flex;
   gap: 6px;
+`;
+
+export const Container = styled.div`
+  display: flex;
+  justify-content: space-between;
 `;
